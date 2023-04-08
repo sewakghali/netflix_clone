10364b0f243b3d210516027d67d1cc47d390fdb4
Sewak Singh Gill <sgill116@myseneca.ca>
2023-04-08 02:40:00 -0400

    finishing touches

--- components/mobileMenu.tsx
+++ components/mobileMenu.tsx
@@ -1,43 +1,55 @@
 import { useRouter } from 'next/router';
 import React from 'react';
-import NavbarItem from './navbarItem';
+import Link from 'next/link';
 
-interface MobileMenuProps{
+interface MobileMenuProps {
   visible?: boolean,
 }
 
-const MobileMenu: React.FC<MobileMenuProps> =({visible})=>{
-  if(!visible) {
+const MobileMenu: React.FC<MobileMenuProps> = ({ visible }) => {
+  if (!visible) {
     return null;
   }
   const router = useRouter();
-  return(
+  return (
     <div className='bg-black w-56 absolute top-8 left-0 py-5 flex-col border-2 border-gray-800 flex'>
       <div className='flex flex-col gap-4'>
-        <div className='px-3 text-cen
-         text-white hover:underline' onClick={()=>{router.push('/')}}>
-          Home
-        </div>
-        <div className='px-3 text-cen
-         text-white hover:underline' onClick={()=>{router.push('/')}}>
-          Series
-        </div>
-        <div className='px-3 text-cen
-         text-white hover:underline' onClick={()=>{router.push('/lol')}}>
-          Films
-        </div>
-        <div className='px-3 text-cen
-         text-white hover:underline' onClick={()=>{router.push('/')}}>
-          New and Popular
-        </div>
-        <div className='px-3 text-cen
-         text-white hover:underline' onClick={()=>{router.push('/')}}>
-          My List
-        </div>
-        <div className='px-3 text-cen
-         text-white hover:underline' onClick={()=>{router.push('/')}}>
-          Browse by Language
-        </div>
+        <Link passHref legacyBehavior href={'/'}>
+          <div className='px-3 text-cen
+         text-white hover:underline'>
+            Home
+          </div>
+        </Link>
+        <Link passHref legacyBehavior href={'/series'}>
+          <div className='px-3 text-cen
+         text-white hover:underline'>
+            Series
+          </div>
+        </Link>
+        <Link passHref legacyBehavior href={'/films'}>
+          <div className='px-3 text-cen
+         text-white hover:underline'>
+            Films
+          </div>
+        </Link>
+        <Link passHref legacyBehavior href={'newMovies'}>
+          <div className='px-3 text-cen
+         text-white hover:underline'>
+            New and Popular
+          </div>
+        </Link>
+        <Link passHref legacyBehavior href={'/myList'}>
+          <div className='px-3 text-cen
+         text-white hover:underline'>
+            My List
+          </div>
+        </Link>
+        <Link passHref legacyBehavior href={'/language'}>
+          <div className='px-3 text-cen
+         text-white hover:underline'>
+            Browse by Language
+          </div>
+        </Link>
       </div>
 
     </div>
