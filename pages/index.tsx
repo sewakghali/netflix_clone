fde953ea8a1c71846cfd27af16bf084fb05bcf3d
Sewak Singh Gill <sgill116@myseneca.ca>
2023-04-09 09:08:02 -0400

    finishing touches

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,11 +7,12 @@ import useMovieList from "@/hooks/useMovieList";
 import useFavourites from "@/hooks/useFavourites";
 import InfoModal from "@/components/infoModal";
 import useInfoModal from "@/hooks/useInfoModal";
+import Head from "next/head";
 
-export async function getServerSideProps(context: NextPageContext){
+export async function getServerSideProps(context: NextPageContext) {
   const session = await getSession(context);
-  if(!session){
-    return{
+  if (!session) {
+    return {
       redirect: {
         destination: '/auth',
         permanent: false,
@@ -24,19 +25,25 @@ export async function getServerSideProps(context: NextPageContext){
 }
 
 export default function Home() {
-  const {data: movies=[]} = useMovieList();
-  const {data:favourites=[]} = useFavourites(); 
+  const { data: movies = [] } = useMovieList();
+  const { data: favourites = [] } = useFavourites();
   const { isOpen, closeModal } = useInfoModal();
 
   return (
     <>
-    <InfoModal visible={isOpen} onClose={closeModal}/>
-    <Navbar/>
-    <Billboard/>
-    <div className="pb-40">
-      <MovieList data={movies} title={`Trending Now`}/>
-      <MovieList data={favourites} title={`My List`}/>
-    </div>
+      <Head>
+        <title>Netflix Clone</title>
+        <meta property="og:NetflixClone" content="Netflix Clone" key="Netflix Clone" />
+        <meta name="google-site-verification" content="phtHwikeVzIrMdg5D5NBaHgbOGZzEnnzUldKWgTNw_g" />
+      </Head>
+
+      <InfoModal visible={isOpen} onClose={closeModal} />
+      <Navbar />
+      <Billboard />
+      <div className="pb-40">
+        <MovieList data={movies} title={`Trending Now`} />
+        <MovieList data={favourites} title={`My List`} />
+      </div>
     </>
   )
 }
